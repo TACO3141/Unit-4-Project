27f436ad362c571b442560588d1652c0e84f0e06
TACO3141 <andrewej10@gmail.com>
2025-11-19 11:35:19 -0700

    Update U4Project.java

--- U4Project.java
+++ U4Project.java
@@ -5,9 +5,10 @@ class U4Project
 {
   public static void main(String[] args)
   {
+    boolean sorted = false;
     String scramble = "";
     Scanner scan = new Scanner(System.in);
-    System.out.println("Exception Caught: Matrix.Backend.Main.UserNotRecognizedException: Biometric failure. Manual Override Intiated.\n...");
+    System.out.println("Exception Caught: Matrix.Admin.Main.UserNotRecognizedException: Biometric failure. Manual Override Intiated.\n...");
     try
       {
         TimeUnit.SECONDS.sleep(3);
@@ -36,11 +37,27 @@ class U4Project
       backwardsPassword = backwardsPassword + arr[i];
     }
     System.out.println("Backwards password:\n" + backwardsPassword + "\n");
-    System.out.println("Enter method:\n{S} - Scramble\n{X} - Sort");
+    System.out.println("Enter method:\n{S} - Scramble\n{B} - Sort");
+    //I chose to make bubble sort for this project. I looked up how exactly bubble sort works, but the code is 100% from my head.
     String choice = scan.nextLine();
     if (choice.toLowerCase() == "s")
     {
-      
+      System.out.println("Scramble initiated.");
+      for (int i = password.length() - 1; i >= 0; i--)
+      {
+        int rand = (int) (Math.random() * password.length());
+        scramble += password.substring(rand, rand + 1);
+        password = password.substring(0, rand) + password.substring(rand + 1);
+      }
+      System.out.println("Password scrambled. Result:\n" + scramble);
+    }
+    else if (choice.toLowerCase() == "b")
+    {
+      char sortArr = password.toCharArray();
+      while (sorted = false)
+      {
+        //Do it bruh 👅
+      }
     }
   }
 }
